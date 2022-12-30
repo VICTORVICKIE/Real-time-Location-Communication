41660f129959db9481a679ab5747c8ccd50c545c
VICTORVICKIE <s.vickie14@gmail.com>
2022-12-30 12:37:46 +0530

    Added PointAnnotation

---
+++ app/src/main/java/dev/victor/locationshareapp/MarkerActivity.kt
@@ -0,0 +1,11 @@
+package dev.victor.locationshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MarkerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marker)
+    }
+}
